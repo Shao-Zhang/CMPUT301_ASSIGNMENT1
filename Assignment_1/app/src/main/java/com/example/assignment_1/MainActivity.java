963ed049839689371256822b2c101ff32ef2c232
ShaoFeng Zhang <shaofengzhang@ShaoFengs-MacBook-Pro.local>
2021-02-06 18:52:56 -0700

    display listview

--- Assignment_1/app/src/main/java/com/example/assignment_1/MainActivity.java
+++ Assignment_1/app/src/main/java/com/example/assignment_1/MainActivity.java
@@ -16,12 +16,13 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     ListView experimentListView;
-    ArrayAdapter<String> experimentAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<Experiment> currentList = AssignmentApplication.getExperiments();
 
         Bundle extras = getIntent().getExtras();
         if (extras != null){
@@ -29,10 +30,12 @@ public class MainActivity extends AppCompatActivity {
             String experimentDate = extras.getString("date");
             String experimentDescription = extras.getString("description");
             Experiment experiment = new Experiment(experimentName, experimentDate, experimentDescription);
-            ArrayList<Experiment> current = AssignmentApplication.getExperiments();
-            current.add(experiment);
+            currentList.add(experiment);
         }
 
+        experimentListView = findViewById(R.id.experimentList);
+        ExperimentAdapterView adapterView = new ExperimentAdapterView(this, currentList);
+        experimentListView.setAdapter(adapterView);
     }
 
     public void addButtonClicked(View view){
