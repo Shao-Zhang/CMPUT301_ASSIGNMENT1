cb374bd1bd165e40fc876407e76863a8b88e84a7
unknown <sfeng.zhang39@gmail.com>
2021-02-10 17:02:29 -0700

    renamed project name

---
+++ ExperimentBook/app/src/main/java/com/example/experimentbook/AddExperimentActivity.java
@@ -0,0 +1,40 @@
+package com.example.experimentbook;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddExperimentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_experiment);
+
+        EditText name = findViewById(R.id.experimentName);
+        EditText date = findViewById(R.id.experimentDate);
+        EditText description = findViewById(R.id.experimentDescription);
+
+        Button addItemButton = findViewById(R.id.addFinalButton);
+        addItemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fullName = name.getText().toString();
+                String fullDate = date.getText().toString();
+                String fullDescription = description.getText().toString();
+                Intent returnToMain = new Intent(AddExperimentActivity.this, MainActivity.class);
+                returnToMain.putExtra("name", fullName);
+                returnToMain.putExtra("date", fullDate);
+                returnToMain.putExtra("description", fullDescription);
+                startActivity(returnToMain);
+            }
+        });
+
+    }
+}
---
+++ ExperimentBook/app/src/main/java/com/example/experimentbook/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.experimentbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView experimentListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayList<Experiment> currentList = ExperimentBookApplication.getExperiments();
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null){
+            String experimentName = extras.getString("name");
+            String experimentDate = extras.getString("date");
+            String experimentDescription = extras.getString("description");
+            Experiment experiment = new Experiment(experimentName, experimentDate, experimentDescription);
+            currentList.add(experiment);
+        }
+
+        experimentListView = findViewById(R.id.experimentList);
+        ExperimentAdapterView adapterView = new ExperimentAdapterView(this, currentList);
+        experimentListView.setAdapter(adapterView);
+
+        experimentListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // open up new activity and pass in the position clicked so we know which experiment it is.
+            }
+        });
+    }
+
+    public void addButtonClicked(View view){
+        Intent intent = new Intent(this, AddExperimentActivity.class);
+        startActivity(intent);
+    }
+
+
+}
